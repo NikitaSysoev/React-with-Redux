90916ee5f109c54311115290677d3d36e90d411f
NikitaSysoev <na.sysoev@gmail.com>
2019-05-27 10:09:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerText: "This is Class based Component"
+    };
+  }
+
+  handleChange = e => {
+    const { target } = e;
+    const { name, value } = target;
+    this.setState(
+      (prevState, prevProps) => ({
+        headerText: value
+      }),
+      () => {
+        console.log("эстейт поменялся", this.state);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.state.headerText}</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="headerText"
+            value={this.state.headerText}
+          />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <App />
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
@@ -23,4 +74,4 @@ function App() {
   );
 }
 
-export default App;
+export default App2;
